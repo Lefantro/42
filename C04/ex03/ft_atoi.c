9d37c8156008e5e73c3e68f93feae0c149f39d15
Andreea Al-Afuni <andreea.afuni@gmail.com>
2023-10-11 15:58:43 +0200

    Fixed atoi function

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,12 +6,19 @@
 /*   By: aafuni <aafuni@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 17:59:08 by aafuni            #+#    #+#             */
-/*   Updated: 2023/10/10 23:34:03 by aafuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <unistd.h>
+
+/*
+32 = space
+\f = form feed = 12
+\n = line feed = 10
+\r = carriage return = 13
+\t = horizontal tab = 09
+\v = vertical tab = 11
+*/
 
 int	ft_atoi(char *str)
 {
@@ -21,32 +28,28 @@ int	ft_atoi(char *str)
 
 	number = 0;
 	minus = 1;
-	while (*str)
+	c = *str;
+	while ((c == 32) || ((c < 8) && (c > 13)) || (c == '+' ) || (c == '-'))
 	{
-		if ((*str == '+') || (*str == ' '))
-			str++;
-		else if (*str == '-')
-		{
+		if (c == '-')
 			minus = minus * (-1);
-			str++;
-		}
-		else if ((*str >= '0') && (*str <= '9'))
-		{
-			c = *str;
-			number = number * 10 + c - '0';
-			str++;
-		}
-		else
-			*str = '\0';
+		str++;
+		c = *str;
 	}
-	return (number * minus);
+	while ((*str >= '0') && (*str <= '9'))
+	{
+		c = *str;
+		number = number * 10 + (c - '0') * minus;
+		str++;
+	}
+	return (number);
 }
 /*
 int	main(void)
 {
 	int	n;
 
-	char str[] = "---+--+1234ab567";
+	char str[] = "---123";
 	n = ft_atoi(str);
 	printf("%i\n", n);
 }
